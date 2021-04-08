b0c15dacc43224812808d5c057b90c0cff8ef25f
José Felipe Theodoro <j219081@dac.unicamp.br>
2021-04-08 16:43:27 -0300

    começo do lab2

---
+++ lab03/AquarioLombriga.java
@@ -0,0 +1,56 @@
+package lab03;
+
+public class AquarioLombriga{
+    int tamAq, tamLomb, pos;
+    int sentido;//verifica pra qual lado esta virado
+
+    AquarioLombriga(int pTamAq, int pTamLomb, int pPos){
+        tamAq = pTamAq;
+        tamLomb = pTamLomb;
+        pos = pPos;
+        sentido = 1;
+    }
+
+    void crescer(){
+        if(pos+tamLomb < tamAq){
+            tamLomb += 1;
+        }
+    }
+
+    void virar(){
+        if(sentido == 1){
+            sentido = -1;
+        }else{
+            sentido = 1;
+        }
+        
+    }
+
+    void mover(){
+        if(pos+tamLomb == tamAq){
+            virar();
+        }else{
+            if(sentido == 1){
+                pos += 1;
+            }else{
+                pos -=1;
+            }
+        }
+    }
+
+    void apresenta(){
+        for(int i = 0; i < tamAq; i++){
+            if(i < pos || i > pos+tamLomb-1){
+                System.out.print("#");
+            }else{
+                if(sentido == -1 && i == pos){
+                    System.out.print("0");
+                }else if(sentido == 1 && i == pos+tamLomb-1){
+                    System.out.print("0");
+                }else{
+                    System.out.print("@");
+                }
+            }
+        }
+    }
+}
---
+++ lab03/README.md
@@ -0,0 +1,2 @@
+##Notebook 1 sobre lombriga:
+  [Notebook 1](notebook/lab-lombriga-ra219081.ipynb)
