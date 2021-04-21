2a28ff9fec219cb0687f2695e617cfe67637e089
José Felipe Theodoro <j219081@dac.unicamp.br>
2021-04-20 22:37:47 -0300

    correçao de problemas em relaçao a leitura csv soh falta arrumar qual arquivo ler o csv

--- lab04/AppRestaUm.java
+++ lab04/AppRestaUm.java
@@ -4,61 +4,19 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class CSVReader {
-	private String dataSource;
-	private String[] commands;
-  
-  public CSVReader() {
-    this.commands = null;
-    this.dataSource = null;
-  }
-
-  public String getDataSource() {
-    return dataSource;
-  }
-
-  public void setDataSource(String dataSource) {
-    this.dataSource = dataSource;
-    if (dataSource == null) {
-      commands = null;
-    } else
-      readCSV();
-  }
-
-  
-  public String[] requestCommands() {
-    return commands;
-  }
-  
-  private void readCSV() {
-    try {
-      BufferedReader file = new BufferedReader(new FileReader(dataSource));
-        
-      String line = file.readLine();
-      if (line != null) {
-        commands = line.split(",");
-        line = file.readLine();
-      }
-      file.close();
-    } catch (IOException erro) {
-      erro.printStackTrace();
-    }
-  }
-  
-}
-
 public class AppRestaUm{
 
-  String executaJogo(){
+  static String executaJogo(){
       CSVReader csv = new CSVReader();
-      csv.setDataSource("../../../lab04/testes/teste0.csv");//modificar isso
+      csv.setDataSource("../../../mc322/lab04/testes/teste0.csv");//modificar isso
       String commands[] = csv.requestCommands();
-      String passos[] = new String[commands.lenght];
+      String passos;
       Tabuleiro tabuleiro = new Tabuleiro();
       char comando[] = new char[5];//pega a string de comando em char
-      for(int i = 0; i < commands.lenght; i++){
+      passos = "";
+      for(int i = 0; i < commands.length; i++){
           comando = commands[i].toCharArray();
-          if(i = 0){
+          if(i == 0){
               System.out.println("Tabuleiro inicial:");
               tabuleiro.saidaGrafico();
               System.out.println();
@@ -67,13 +25,13 @@ public class AppRestaUm{
           System.out.println("Source: " + comando[0] + comando[1]);
           System.out.println("Target: " + comando[3] + comando[4]);
           tabuleiro.saidaGrafico();
-          passos[i] = tabuleiro.saidaVetor();
+          passos += tabuleiro.saidaVetor();
       }
       return passos;
   }
 
-  public static void main(){
-      String passos[];
-      passos = executarJogo();
+  public static void main(String args[]){
+      String passos;
+      passos = executaJogo();
   }
 }
