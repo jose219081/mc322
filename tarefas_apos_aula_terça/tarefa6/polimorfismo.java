66bad040d9541d2345bc4576a3ea1b0d8483bc60
José Felipe Theodoro <j219081@dac.unicamp.br>
2021-04-27 22:56:51 -0300

    falta tarefa do polimorfismo e checar a resposta do herança

---
+++ tarefas_apos_aula_terça/tarefa6/polimorfismo.java
@@ -0,0 +1,55 @@
+public class Lista {
+    protected int vlista[] = new int[100];
+    protected int ultimo = -1;
+    
+    public void adicionar(int item) {
+       if (ultimo+1 < vlista.length) {
+          ultimo++;
+          vlista[ultimo] = item;
+       }
+    }
+    
+    public String toString() {
+       String str = "";
+       for (int l = 0; l <= ultimo; l++)
+          str += vlista[l] + ((l < ultimo) ? ", " : "");
+       return str;
+    }
+}
+////////////////////////////////////////////////////////////////
+
+public class ListaUtil extends Lista {
+
+    public void ordena() {
+       int lastSwap = ultimo;
+       while (lastSwap > 0) {
+          int swap = 0;
+          for (int v = 0; v < lastSwap; v++)
+             if (vlista[v] > vlista[v+1]) {
+                int aux = vlista[v];
+                vlista[v] = vlista[v+1];
+                vlista[v+1] = aux;
+                swap = v;
+             }
+           lastSwap = swap;
+       }
+    }
+ 
+}
+///////////////////////////////////////////////////
+
+public class ListaUtilNoDup extends ListaUtil {
+    public void ordena() {
+      super.ordena();
+       
+      int s = 0;
+      for (int v = 0; v <= ultimo; v++)
+         if (vlista[s] != vlista[v]) {
+            s++;
+            vlista[s] = vlista[v];
+         }
+      ultimo = s;
+    }
+}
+//////////////////////////////////////////////////////
+
